34a758a2bf72f41b90d7c65788c8f0c3e3230583
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-08 14:50:57 +0100

    modified

---
+++ name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+my_name="Mesgna"
+readonly my_age="40" 
+echo "My name is $my_name"
+echo "I'am $my_age old."
+my_age="50"
+echo $my_age
+# readonly variables can not be reassingedhello_world.sh
+date=$(date)
+echo the time now is $date
