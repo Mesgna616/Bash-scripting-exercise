a98132969cb7e62e90d4a943f22ee6a0bc209029
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-08 15:07:26 +0100

    cleaned up

--- variables/name.sh
+++ variables/name.sh
@@ -6,4 +6,5 @@ echo "I'am $my_age old."
 my_age="50"
 echo $my_age
 # readonly variables can not be reassingedhello_world.sh
-
+date=$(date)
+echo the time now is $date
---
+++ variables/sub-shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+name="Mesgna Mehari"
+now=$(date)
+echo  " Hello $name"
+echo " The system time and date is :"
+echo $now
+echo " Your user name is  : $USER"
+
