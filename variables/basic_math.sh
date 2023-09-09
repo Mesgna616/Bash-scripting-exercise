9ea9847af22458cd8246a8342e886f88329ba582
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-09 14:40:22 +0100

    math

--- variables/basic_math.sh
+++ variables/basic_math.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 var1=10
 var2=10
-expr var1 + var2
+
+# Addition
 result=$(expr $var1 + $var2)
-echo "The result is: $result"
+echo "The result of addition is: $result"
+
+# Subtraction
+result2=$(expr $var1 - $var2)
+echo "The result of subtraction is: $result2"
+
+# addition result 
+result3=$(expr $result1 + $result2)
+echo " This is the aggergate of the two sum : $result3"
