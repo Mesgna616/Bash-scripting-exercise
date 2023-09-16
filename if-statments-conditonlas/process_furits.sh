f9ca1fff862ad2bc93d8c1d23239161ee5ad9db3
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-16 15:29:03 +0100

    rename

--- if-statments-conditonlas/process_furits.sh
+++ if-statments-conditonlas/process_furits.sh
@@ -7,6 +7,8 @@ if grep -q "Mango" "$input_file"; then
     # If "Mango" is present, use sed to replace it with "Watermelon"
     sed 's/Mango/Watermelon/g' "$input_file" > "output.txt"
 else
-    # If "Mango" is not present, just copy the line as is
+    # If "Mango" is not present, display a message and copy the line as is
+    echo "Mango is not found"
     cp "$input_file" "output.txt"
 fi
+
