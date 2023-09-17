de46c27a67b6e3ff6184a1bfb7a0070acaaf3ce8
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-17 11:03:47 +0100

    modification

--- if-statments-conditonlas/process_furits.sh
+++ if-statments-conditonlas/process_furits.sh
@@ -4,7 +4,7 @@ input_file="furits.txt"
 
 # Use grep to check if "Mango" is present in the line
 if grep -q "Mango" "$input_file"; then
-    # If "Mango" is present, use sed to replace it with "Watermelon"
+    # If "Mango" is present, use sed to replace it with "Watermelon" and we use the g to set it globally 
     sed 's/Mango/Watermelon/g' "$input_file" > "output.txt"
 else
     # If "Mango" is not present, display a message and copy the line as is
