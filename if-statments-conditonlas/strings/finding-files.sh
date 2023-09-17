7c4ad5df7197b4ffdd6d68552bceae5a353a5b92
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-17 14:34:50 +0100

    modified

---
+++ if-statments-conditonlas/strings/finding-files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if a filename argument is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+filename="$1"
+directory="/mnt/c/users/mesgn/onedrive/documents/Bash-scripting-exercise"
+
+# Use the 'find' command to search for the file in the specified directory
+found_files=$(find "$directory" -type f -name "$filename" 2>/dev/null)
+
+if [ -n "$found_files" ]; then
+  echo "File(s) found:"
+  echo "$filename"
+else
+  echo "File not found: $filename"
+fi
