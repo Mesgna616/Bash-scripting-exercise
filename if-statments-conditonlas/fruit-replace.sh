c8f9baa8f2f28405771506fd59fff85c3debcbf0
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-09-30 15:27:46 +0100

    refactored

--- if-statments-conditonlas/fruit-replace.sh
+++ if-statments-conditonlas/fruit-replace.sh
@@ -5,7 +5,7 @@ if [ -e "$input_file" ]; then
 if grep -qE  'Mango|Ornage' "$input_file" ; then
  # Use sed to perform the replacements and save the result to a new file
 sed -e 's/Mango/Strawberry/g; s/Orange/Blueberry/g'  "$input_file" >  new_fruits.txt 
-else echo "No occurrences of 'Mango' or 'Orange' found in "$input_file""
+else echo "No occurrences of 'Mango' or 'Orange' found in "$input_file" "
 fi
 else 
 echo " No file by the name "$input_file" exists"
