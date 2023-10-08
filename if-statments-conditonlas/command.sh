b0f52efc3e82635599b0452a8607b9fecb2b0747
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-10-08 16:52:29 +0100

    if statments

---
+++ if-statments-conditonlas/command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+command=htop
+if command -v $command 
+then 
+echo "$command is available lets run it ..."
+else 
+echo "$command is not avilable ,installing it ..."
+sudo apt update && sudo apt install  -y $command
+fi
+$command
