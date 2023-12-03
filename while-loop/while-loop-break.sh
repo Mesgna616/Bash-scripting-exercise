15b6be688045c048a8a0aadefd97d73f4750b76b
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-12-03 13:39:55 +0000

    loop with break

--- while-loop/while-loop-break.sh
+++ while-loop/while-loop-break.sh
@@ -3,14 +3,21 @@
 while true; do
     read -p "Enter two numbers (-1 to quit): " a b
 
-    if [[ $a == "-1" ]]; then
-        break
-    elif ! [[ $a =~ ^-?[0-9]+$ && $b =~ ^-?[0-9]+$ ]]; then
-        echo "Invalid input. Please enter valid numeric values or -1 to quit."
-        continue
-    elif ((a == 0 || b == 0)); then
+    # Check if both inputs are non-numeric or empty
+    if ! [[ $a =~ ^-?[0-9]+$ && $b =~ ^-?[0-9]+$ ]]; then
+        if [[ $a == "-1" ]]; then
+            break
+        else
+            echo "Invalid input. Please enter valid numeric values or -1 to quit."
+            continue
+        fi
+    fi
+
+    if [[ $a -eq 0 || $b -eq 0 ]]; then
         echo "Neither number can be zero."
         continue
+    elif [[ $a == "-1" ]]; then
+        break
     else
         ans=$((a + b))
         echo "The sum of $a and $b is: $ans"
